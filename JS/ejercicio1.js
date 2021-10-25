b78e666504041521b70e17813e5af30675d66f51
Danfelmor <91349544+Danfelmor@users.noreply.github.com>
2021-10-25 14:19:26 -0500

    Create ejercicio1.js

---
+++ JS/ejercicio1.js
@@ -0,0 +1,7 @@
+function  cont ( ) {
+setTimeout ( function ( ) {
+    deje  valor = documento . getElementById ( 'cont' ) ;
+    vamos  cantidad = valor . valor . longitud ;
+    documento . getElementById ( 'RES' ) . innerHTML = cantidad + "caracteres" ;
+} ) ;
+}
